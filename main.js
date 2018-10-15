395fd2e074e9b7e73b125889cc5f267fa024bb13
Smaker <jiheon.unidev@gmail.com>
2018-10-16 02:17:16 +0900

    Add materializecss

--- main.js
+++ main.js
@@ -3,7 +3,8 @@ const {app, BrowserWindow} = require('electron')
 let mainWindow
 
 function createWindow () {
-  mainWindow = new BrowserWindow({width: 800, height: 600})
+  mainWindow = new BrowserWindow({width: 400, height: 500})
+  mainWindow.setMenu(null);
 
   mainWindow.loadFile('./app/src/index.html')
   mainWindow.on('closed', function () {
