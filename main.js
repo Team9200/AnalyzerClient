ff8ca5eb50326e044d1f06b7725e70b763337d83
true13 <true1982@naver.com>
2018-11-17 11:36:54 +0900

    mid1

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
-const {
-  app,
-  ipcMain
-} = require('electron')
+const { app, ipcMain } = require('electron')
 const window = require('./window');
+const fs = require('fs');
+
+let receivedData = new Array();
+let resultData;
 
 app.on('ready', window.createMainWindow);
 
@@ -26,4 +27,24 @@ ipcMain.on('menu', function (event, message) {
     default:
       break;
   }
+});
+
+ipcMain.on('write', () => {
+  window.createReportWindow();
+});
+
+ipcMain.on('requestFile', () => {
+  window.createReqWindow();
+});
+
+ipcMain.on('receiveFile', (event, message) => {
+	receivedData = receivedData.concat(message.binary.data);
+
+	if(receivedData.length == message.size) {
+		console.log("test", receivedData);
+		resultData = new Buffer.from(receivedData);
+		fs.writeFileSync('./samples/sample' + new Date().getTime() + '.zip', resultData);
+		event.sender.send('receivedFile-reply', 'test');
+		console.log('test!');	
+	}
 });
--- window.js
+++ window.js
@@ -1,21 +1,33 @@
-const {
-    app,
-    BrowserWindow
-} = require('electron');
+const { app, BrowserWindow } = require('electron');
+const fs = require('fs');
+const url = require('url');
+const path = require('path');
 
 let mainWindow;
 let reportWindow;
+let reqWindow;
 
 module.exports = {
     createMainWindow: () => {
         mainWindow = new BrowserWindow({
-            width: 400,
-            height: 500,
+            width: 600,
+            height: 400,
             resizable: false
         });
         mainWindow.setMenu(null);
-        
+
         mainWindow.loadFile('./app/src/index.html')
+
+        mainWindow.webContents.openDevTools();
+
+        mainWindow.webContents.on('did-finish-load', () => {
+            var fileList;
+
+            fileList = fs.readdirSync('./samples');
+            console.log(fileList);
+            mainWindow.webContents.send("fileList", fileList);
+        });
+
         mainWindow.on('closed', function () {
             mainWindow = null;
         });
@@ -23,8 +35,8 @@ module.exports = {
     createReportWindow: () => {
         reportWindow = new BrowserWindow({
             parent: mainWindow,
-            width: 400,
-            height: 500,
+            width: 250,
+            height: 300,
             resizable: false
         });
         reportWindow.setMenu(null);
@@ -32,6 +44,22 @@ module.exports = {
         reportWindow.loadFile('./app/src/reportForm.html')
         reportWindow.on('closed', function () {
             reportWindow = null;
+        });
+    },
+
+    createReqWindow: () => {
+        reqWindow = new BrowserWindow({
+            parent: mainWindow,
+            width: 0,
+            height: 0,
+            resizable: false
+        });
+        reqWindow.setMenu(null);
+
+        reqWindow.loadFile('./app/src/req.html');
+
+        reqWindow.on('closed', function () {
+            reqWindow = null;
         });
     }
 }
